8c957699f4252a355d30db56f34b453fd5b6402c
selvadhanush <selva.dhanush.s81@kalvium.community>
2025-05-09 14:45:27 +0530

    deploy-back

--- server/app.js
+++ server/app.js
@@ -20,8 +20,10 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api", publicRoutes);
 app.use("/api/auth", authRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/users', userRoutes);
-
+// app.use('/api/users', userRoutes);
+app.get('/',(req,res)=>{
+  res.status(200).send('Hello from backend')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
